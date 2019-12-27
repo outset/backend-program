86362040a6221128b5495fe3e486159300b93643
Simon-Nie <niezhm@163.com>
2019-12-27 21:54:45 +0800

    use lombok data

--- demo/src/main/java/com/example/demo/Model/User.java
+++ demo/src/main/java/com/example/demo/Model/User.java
@@ -1,5 +1,8 @@
 package com.example.demo.Model;
 
+import lombok.Data;
+
+@Data
 public class User {
     private String uid;
 
@@ -12,52 +15,4 @@ public class User {
     private String avatarurl;
 
     private Byte gender;
-
-    public String getUid() {
-        return uid;
-    }
-
-    public void setUid(String uid) {
-        this.uid = uid == null ? null : uid.trim();
-    }
-
-    public String getOpenid() {
-        return openid;
-    }
-
-    public void setOpenid(String openid) {
-        this.openid = openid == null ? null : openid.trim();
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname == null ? null : nickname.trim();
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token == null ? null : token.trim();
-    }
-
-    public String getAvatarurl() {
-        return avatarurl;
-    }
-
-    public void setAvatarurl(String avatarurl) {
-        this.avatarurl = avatarurl == null ? null : avatarurl.trim();
-    }
-
-    public Byte getGender() {
-        return gender;
-    }
-
-    public void setGender(Byte gender) {
-        this.gender = gender;
-    }
 }
